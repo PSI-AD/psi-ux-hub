ad503951b4fe4b598f59358f500becd9483b9d7f
Amr Ibrahim <amribrahim@Amrs-MacBook-Air.local>
2026-01-15 21:05:59 +0200

    emergency: nuclear reset of entry paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import './index.css' // Commented out to ensure no CSS crash for now, or per user generic 'strictly looks like this' (User showed import './index.css', so I'll include it IF it exists. I'll include it.)
-// Actually user request:
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import './index.css'
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode><App /></React.StrictMode>
-// )
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
